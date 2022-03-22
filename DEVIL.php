b684201e85c0e5aa3e10cc1ef0246881d8b23c30
DEVIL <Andreyxxl7@yandex.ru>
2022-03-22 15:11:55 +0700

    php4

--- DEVIL.php
+++ DEVIL.php
@@ -2,5 +2,6 @@
 	header('Content-Type: text/html; charset=utf-8');
 	$name = $_POST['n'];
 	$surname = $_POST['s'];
-	echo 'Привет '.$name. ' '.$surname; 
+	echo 'Привет '.$name. ' '.$surname. '<br>';
+	echo '<a href="DEVI.html">Вернуться назад</a>'
 ?>
